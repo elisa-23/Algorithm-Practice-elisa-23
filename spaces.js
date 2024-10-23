06d425edbf6a71007588c3928a97c7e736f73b1b
elisa-23 <158204265+elisa-23@users.noreply.github.com>
2024-10-23 12:18:34 -0400

    sdgfdgdf

---
+++ spaces.js
@@ -0,0 +1,28 @@
+function parkingSpaces(N,yesterday,today) {
+    let occupied = 0;
+    if (yesterday.length != N && today.length != N) {
+        return "ERROR: You recorded different sets of parking spaces. (not the same amount)";
+    } else {
+        for (let i=0; i<N; i++) {
+            console.log(yesterday[i] + " " + today[i]);
+            if (yesterday[i] === today[i]) {
+                if (yesterday[i] === "C") {
+                    occupied++;
+                }else {
+                    console.log("This parking spot was never occupied today or yesterday.");
+                }
+            }else if ((yesterday[i] != "C" && yesterday[i] != ".") || (today[i] != "C" && today[i] != ".")) {
+                return "ERROR: You recorded the data wrong for yesterday or today.";
+            }else {
+                if (yesterday[i] === ".") {
+                    console.log("This parking spot wasn't occupied yesterday.");
+                }else {
+                    console.log("This parking spot wasn't occupied today.");
+                }
+            }
+        }
+        return occupied + " spaces were occupied today and yesterday.";
+    }
+}
+
+console.log(parkingSpaces(0, "", ""));
